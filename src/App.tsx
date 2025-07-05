b0c416f784def4c07254a2dd43b2d6c422b963fa
pratikdp707 <pratikparate707@gmail.com>
2025-07-05 23:38:51 +0530

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <div>
+            <h1 className="text-3xl font-bold underline text-sky-500">
+                Hello world!
+            </h1>
+            <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo"/>
+            </a>
+            <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo"/>
+            </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
