d189b755bbb1d5cc2ce582f6ec36bf2cac42818d
pratikdp707 <pratikparate707@gmail.com>
2025-07-19 20:26:54 +0530

    services: made effect little smooth and change function definition for Services

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,20 +1,19 @@
 import '../../App.css'
+import data from '../../constants/constants.json'
 
-function Services() {
-  const pageHeading = "SERVICES";
-
+const Services = () => {
   return (
     <>
       <div className="p-10">
         <div className="text-2xl font-bold w-full tracking-[3px] text-primary-accent-color">
-          {pageHeading.split("").map((char, i) => (
-            <span className="animated-letter" key={i} style={{ animationDelay: `${i * 0.3}s` }}>
+          {data?.services?.heading?.split("").map((char, i) => (
+            <span className="animated-letter" key={i} style={{ animationDelay: `${i * 0.2}s` }}>
               {char}
             </span>
           ))}
         </div>
         <div className="text-5xl font-bold mt-10">
-          Tailored Services For Your Dreams
+          {data?.services?.subheading}
         </div>
       </div>
     </>
