425e80ef994d784aa3520b06296afba3c62bb0a8
pratikdp707 <pratikparate707@gmail.com>
2025-07-19 21:46:15 +0530

    navbar: made the animation little smooth and changed backrgound color

--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -1,43 +1,66 @@
 import { useState } from "react";
-import './navbar.css';
+import "./navbar.css";
 import { Menu, X } from "lucide-react";
-import CadicientLogo from '../../assets/CADICIENT.png';
-import constants from '../../constants/constants.json'
+import CadicientLogo from "../../assets/CADICIENT.png";
+import constants from "../../constants/constants.json";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <>
-    <div className="bg-primary-accent-color sticky top-0 z-50">
-      <div className="bg-primary-dark-color h-[100px] w-full lg:px-[100px] md:px-[20px] px-6 flex items-center justify-between">
-        {/* Logo */}
-        <img src={CadicientLogo} alt="Logo" className="h-[50px]" />
+  return (
+    <>
+      {/* Top navbar container (always on top) */}
+      <div className="bg-primary-accent-color sticky top-0 z-50">
+        <div className="bg-neutral-950 h-[100px] w-full lg:px-[100px] md:px-[20px] px-6 flex items-center justify-between relative">
+          {/* Logo */}
+          <img src={CadicientLogo} alt="Logo" className="h-[50px] z-50" />
 
-        {/* Desktop Menu */}
-        <div className="hidden md:flex text-white text-l space-x-6 font-semibold justify-start">
-          {constants.navbar.map((navbarItem, index) => (
-            <span className="navbar-items underline-hover" key={index}>{navbarItem}</span>
-          ))}
-        </div>
+          {/* Desktop Menu */}
+          <div className="hidden md:flex text-white text-l space-x-6 font-semibold justify-start">
+            {constants.navbar.map((navbarItem, index) => (
+              <span className="navbar-items underline-hover" key={index}>
+                {navbarItem}
+              </span>
+            ))}
+          </div>
 
-        {/* Hamburger for Mobile */}
-        <div className="md:hidden text-white">
-          <button onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <X size={28} /> : <Menu size={28} />}
-          </button>
+          {/* Hamburger for Mobile */}
+          <div className="md:hidden text-white z-50">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? <X size={28} /> : <Menu size={28} />}
+            </button>
+          </div>
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {isOpen && (
-        <div className="md:hidden bg-primary-dark-color text-white flex flex-col items-center py-4 space-y-4 text-lg font-semibold">
+      {/* Slide-in Mobile Drawer (below top bar) */}
+      <div
+        className={`fixed top-[50px] left-0 h-full w-[75%] max-w-sm bg-neutral-950 text-white transform transition-transform duration-500 ease-in-out z-40 shadow-lg ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex flex-col space-y-6 text-xl font-semibold mt-20 px-[30px]">
           {constants.navbar.map((navbarItem, index) => (
-            <span className="navbar-items w-full text-left px-[30px]" key={index}>{navbarItem}</span>
+            <span
+              className="navbar-items text-left"
+              key={index}
+              onClick={() => setIsOpen(false)}
+            >
+              {navbarItem}
+            </span>
           ))}
         </div>
+      </div>
+
+      {/* Backdrop */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-40 z-30 transition-opacity duration-300"
+          onClick={() => setIsOpen(false)}
+        ></div>
       )}
-    </div>
-  </>
+    </>
+  );
 };
 
 export default Navbar;
