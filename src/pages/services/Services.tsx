414d6dbb6229e3359b338cb3307d9a90e79eb43c
pratikdp707 <pratikparate707@gmail.com>
2025-07-19 21:13:39 +0530

    services: moved heading to a common component

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,16 +1,12 @@
-import '../../App.css'
-import data from '../../constants/constants.json'
+import { Heading } from "../../components/Heading/Heading";
+import data from "../../constants/constants.json";
 
 const Services = () => {
   return (
     <>
       <div className="p-10">
-        <div className="text-2xl font-bold w-full tracking-[3px] text-primary-accent-color">
-          {data?.services?.heading?.split("").map((char, i) => (
-            <span className="animated-letter" key={i} style={{ animationDelay: `${i * 0.2}s` }}>
-              {char}
-            </span>
-          ))}
+        <div className="flex w-full justify-center">
+          <Heading heading={data?.services?.heading} />
         </div>
         <div className="text-5xl font-bold mt-10">
           {data?.services?.subheading}
@@ -18,6 +14,6 @@ const Services = () => {
       </div>
     </>
   );
-}
+};
 
 export default Services;
