15dd6de2919c7af3566941c0916841e69fc259ab
Immanuel <purisaurabh2885@gmail.com>
2025-07-21 00:21:33 +0530

    feat: add contact , home and softwares page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { ContactUs } from "./pages/contactus/ContactUs.tsx";
+
+import { Home } from "./pages/home/Home.tsx";
 import Navbar from "./pages/navbar/Navbar.tsx";
+import { Softwares } from "./pages/softwares/Softwares.tsx";
 
 function App() {
-    return (
-        <>
-            <Navbar/>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Softwares />
+      <ContactUs />
+    </>
+  );
 }
 
-export default App
+export default App;
